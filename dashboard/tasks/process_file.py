dd1727f155e9136f2dace7cf201527d5b509b4e6
Douglas <msantos.douglas@gmail.com>
2018-10-02 15:47:36 -0300

    Fix update_db call on task

--- dashboard/tasks/process_file.py
+++ dashboard/tasks/process_file.py
@@ -38,46 +38,12 @@ class ProcessFile(Task):
 
         logger.info("Start process file %s" % uploaded_file)
 
-        # Reads a xslx file
-        wb = load_workbook(filename=uploaded_file)
-        simulate_proc(10, uploaded_file)
-
-        # get the Sheet1 data
-        sheet_ranges = wb['Sheet1']
-
-        # serialize cells data to Dict
-        rows = []
-        for cells in sheet_ranges:
-            rows.append(dict(product=cells[0].value,
-                             category=cells[1].value,
-                             units_sold=cells[2].value,
-                             cost_price=cells[3].value,
-                             total_sold=cells[4].value,
-                             )
-                        )     
-        
         simulate_proc(30, uploaded_file)        
-
-        # insert category in database
-        '''
-        for i, d in enumerate(rows):
-            logger.info("adding category to db %d" % (i))
-            category = CategoryModel()
-            category.name = d['category']
-            category.save()
-        '''
-
         simulate_proc(60, uploaded_file)
         simulate_proc(90, uploaded_file)
         simulate_proc(100, uploaded_file)
 
-        '''
-        #it works - ok
-        category = CategoryModel()
-
-        category.name = 'new category'
-        category.save()
-        '''
+        self.update_db(uploaded_file, company_name)
 
         return 'SUCCESS'
 
