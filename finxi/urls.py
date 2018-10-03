a5ffd0195f47872fb520e037d03da44a04ffbd8c
Douglas <msantos.douglas@gmail.com>
2018-10-03 15:08:10 -0300

    Set default root url to /dashboard/

--- finxi/urls.py
+++ finxi/urls.py
@@ -19,5 +19,6 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('dashboard/', include('dashboard.urls')),
+    path('', include('dashboard.urls')),
 
 ]
